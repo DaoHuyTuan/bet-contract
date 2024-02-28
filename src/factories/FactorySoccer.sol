7a268ad6b98d888e8cbd3cf4df0816e8589e1e5d
zyye-brototype <tuan.dao@brototype.xyz>
2024-02-28 17:49:14 +0700

    update factory

--- src/factories/FactorySoccer.sol
+++ src/factories/FactorySoccer.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.24;
 import "../state/StateSoccer.sol";
 import "../pools/PoolSoccer.sol";
 import "../services/ServiceSoccer.sol";
+import "../types/factories/soccer.sol";
 
 contract FactorySoccer {
     address public owner;
     address[] private states;
     address[] private pools;
     address[] private services;
+    address[3] private composer;
     constructor() {
         owner = msg.sender;
     }
@@ -21,24 +23,28 @@ contract FactorySoccer {
         _;
     }
 
-    function init(string memory _team_name_1, string memory _team_name_2) external payable onlyOwner("Init") {
+    function create_fool() external payable returns (address contract_address) {
         PoolSoccer newPoolSoccer = new PoolSoccer();
-        pools.push(address(newPoolSoccer));
-        ServiceSoccer newServiceSoccer = new ServiceSoccer();
-        services.push(address(newServiceSoccer));
-        address[] memory composer = new address[](2); 
-        composer[0] = address(newPoolSoccer);
-        composer[1] = address(newServiceSoccer);
-        StateSoccer newStateSoccer = new StateSoccer(_team_name_1, _team_name_2, composer);
-        states.push(address(newStateSoccer));
-        emit GameCreated(address(newPoolSoccer), address(newServiceSoccer), address(newStateSoccer));
+        return address(newPoolSoccer);
     }
 
-    function create_fool() external payable onlyOwner("Create Pool contract") {}
+    function create_service() external payable returns (address contract_address) {
+        ServiceSoccer newServiceSoccer = new ServiceSoccer();
+        return address(newServiceSoccer);
+    }
 
-    function create_state() external payable onlyOwner("Create State contract") {} 
-
-    function create_store() external payable onlyOwner("Create store contract") {} 
+    function init(string memory _team_name_1, string memory _team_name_2) external payable onlyOwner("Init") {
+        address[3] memory _composer;
+        address pool_contract = this.create_fool();
+        address service_contract = this.create_service();
+        StateSoccer state_contract = new StateSoccer();
+        _composer[0] = pool_contract;
+        _composer[1] = service_contract;
+        _composer[2] = address(state_contract);
+        state_contract.update_composer(_composer);
+        state_contract.update_teams(_team_name_1, _team_name_2);
+        states.push(address(state_contract));
+    }
 
     function get_states_address() external view returns (address[] memory) {
         return states;
--- src/state/StateSoccer.sol
+++ src/state/StateSoccer.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
+import "../types/factories/soccer.sol";
 
-contract StateSoccer {
+contract StateSoccer is IStateSoccer{
   string private team1;
   string private team2;
   address[] private composes;
-  
-  constructor(string memory _team1, string memory _team2, address[] memory _composes) {
+
+  function update_composer(address[3] memory _composer) public payable  {
+    composes = _composer;
+  }
+
+  function update_teams(string memory _team1, string memory _team2) external payable {
     team1 = _team1;
     team2 = _team2;
-    composes = _composes;
   }
 
   function get_names() public view returns (string memory, string memory) {
